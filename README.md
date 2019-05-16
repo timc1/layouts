87894c260a7802641104febd21b607f012fa4613
Tim <timchang.tcc@gmail.com>
2019-05-16 16:14:04 -0700

    Add home

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
   Layout Experiments
   <br>
 </h1>
-<p align="center" style="font-size: 1.2rem;">Recreating inspirational user interfaces</p>
+<p align="center" style="font-size: 1.2rem;">Recreating inspirational user interfaces 🏙</p>
 
 ## About
 
-The best way to learn and get better at building user interfaces is discovering designs that inspire you and
-recreating it using the tools you know alongside an open mind.
-
-This project is a collection of designs I find fascinating, and my attempts at recreating them.
+This project is a collection of UI designs I find fascinating, and my attempts at recreating them.
--- src/home.tsx
+++ src/home.tsx
@@ -1,5 +1,65 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-  return <div>home</div>
+  return (
+    <Container>
+      <h1>Layout Experiments</h1>
+      <p>Recreating really awesome, unique user interfaces</p>
+      <p>
+        <a
+          href="https://twitter.com/timcchang"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @timcchang
+        </a>
+      </p>
+
+      <ul>
+        <li>
+          <p
+            style={{
+              marginBottom: '10px',
+              textTransform: 'uppercase',
+              fontSize: '12px',
+            }}
+          >
+            Canal Street Market
+          </p>
+          <Link
+            to="/canalstreet.market/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            canalstreet.market
+          </Link>
+        </li>
+      </ul>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  padding: 100px;
+
+  > h1 {
+    margin-bottom: 20px;
+    font-size: 30px;
+  }
+
+  > p {
+    margin-bottom: 20px;
+  }
+
+  > ul {
+    list-style: none;
+    margin-top: 100px;
+  }
+
+  a {
+    color: blue;
+    text-decoration: none;
+  }
+`
